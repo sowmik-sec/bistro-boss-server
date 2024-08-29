adfe8b2916e3bb833bf78ed20b291ee067934823
sowmik-sec <sowmik.sec@gmail.com>
2024-08-29 09:24:30 +0600

    create api for getting reviews

--- index.js
+++ index.js
@@ -23,8 +23,13 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const menuCollection = client.db("bistroDb").collection("menu");
+    const reviewCollection = client.db("bistroDb").collection("reviews");
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
       res.send(result);
     });
   } finally {
