c18104dd182ebadf2d9c288f3e15597a3f4bf21b
sowmik-sec <sowmik.sec@gmail.com>
2024-08-29 09:06:49 +0600

    project initialized

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xgh8h2c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("boss is sitting");
+});
+
+app.listen(port, () => {
+  console.log(`Bistro boss is sitting on port ${port}`);
+});
