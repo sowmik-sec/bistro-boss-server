7a573685ab550f6ab6ccc93cd006d2fbbe90ceb0
sowmik-sec <sowmik.sec@gmail.com>
2024-08-30 11:09:51 +0600

    get cart info of a specific user

--- index.js
+++ index.js
@@ -34,7 +34,9 @@ async function run() {
       res.send(result);
     });
     app.get("/carts", async (req, res) => {
-      const result = await cartCollection.find().toArray();
+      const email = req.query.email;
+      const query = { email };
+      const result = await cartCollection.find(query).toArray();
       res.send(result);
     });
     app.post("/carts", async (req, res) => {
