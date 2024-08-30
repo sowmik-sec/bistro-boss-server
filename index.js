0a1cb65b850e4f9bc56046b54c436edf79edfe0d
sowmik-sec <sowmik.sec@gmail.com>
2024-08-30 10:57:37 +0600

    create api for getting all cart data

--- index.js
+++ index.js
@@ -33,6 +33,10 @@ async function run() {
       const result = await reviewCollection.find().toArray();
       res.send(result);
     });
+    app.get("/carts", async (req, res) => {
+      const result = await cartCollection.find().toArray();
+      res.send(result);
+    });
     app.post("/carts", async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
