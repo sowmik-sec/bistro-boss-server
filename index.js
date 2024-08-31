20477843960c26ca4a5cf2c3ebd6a77621c6b57b
sowmik-sec <sowmik.sec@gmail.com>
2024-08-31 16:53:21 +0600

    create api for getting all users

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ async function run() {
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
     // user related apis
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
