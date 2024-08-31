16fb2c39798bd708bbde077cb3b8bc7ec5ff31a6
sowmik-sec <sowmik.sec@gmail.com>
2024-08-31 19:35:36 +0600

    create api for checking admin

--- index.js
+++ index.js
@@ -50,6 +50,19 @@ async function run() {
       res.send({ token });
     });
     // user related apis
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "unauthorized access" });
+      }
+      const query = { email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
+    });
     app.get("/users", verifyToken, async (req, res) => {
       console.log(req.headers);
       const result = await userCollection.find().toArray();
