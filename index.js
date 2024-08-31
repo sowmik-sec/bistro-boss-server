78a1062394f0200a03a2dbcb3e5cba15aedaf433
sowmik-sec <sowmik.sec@gmail.com>
2024-08-31 16:59:12 +0600

    delete user from db

--- index.js
+++ index.js
@@ -41,6 +41,13 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
     // menu apis
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
