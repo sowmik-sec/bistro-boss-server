2a9155eef4ab257b12a0a03aa1441f4ab033f29e
sowmik-sec <sowmik.sec@gmail.com>
2024-09-01 20:46:07 +0600

    Save menu item to the server and make api secure

--- index.js
+++ index.js
@@ -119,6 +119,11 @@ async function run() {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
+    app.post("/menu", verifyToken, verifyAdmin, async (req, res) => {
+      const item = req.body;
+      const result = await menuCollection.insertOne(item);
+      res.send(result);
+    });
     // reviews apis
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
