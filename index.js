2381193c8dfc53b7fbc2552c2617ee11c0250ae7
sowmik-sec <sowmik.sec@gmail.com>
2024-09-01 22:53:26 +0600

    bug fixed: add / to endpoint

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ async function run() {
       const result = await menuCollection.insertOne(item);
       res.send(result);
     });
-    app.delete("/menu:id", verifyToken, verifyAdmin, async (req, res) => {
+    app.delete("/menu/:id", verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await menuCollection.deleteOne(query);
