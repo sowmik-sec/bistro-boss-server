d36b656bc5651b1d507f39129011d5d7401c5c33
sowmik-sec <sowmik.sec@gmail.com>
2024-09-01 22:46:34 +0600

    Delete menu item with VerifyAdmin middleware

--- index.js
+++ index.js
@@ -124,6 +124,12 @@ async function run() {
       const result = await menuCollection.insertOne(item);
       res.send(result);
     });
+    app.delete("/menu:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    });
     // reviews apis
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
