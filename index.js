723ce312ae60d754b248a7c24ae455818c15b84a
sowmik-sec <sowmik.sec@gmail.com>
2024-09-02 11:33:40 +0600

    get payments of a single user

--- index.js
+++ index.js
@@ -192,6 +192,14 @@ async function run() {
         clientSecret: paymentIntent.client_secret,
       });
     });
+    app.get("/payments/:email", verifyToken, async (req, res) => {
+      const query = { email: req.params.email };
+      if (req.params.email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    });
     app.post("/payments", async (req, res) => {
       const payment = req.body;
       const paymentResult = await paymentCollection.insertOne(payment);
