6265ea036641686a6acc92243983ab523164ea64
sowmik-sec <sowmik.sec@gmail.com>
2024-09-02 20:22:08 +0600

    create api for Getting Order Quantity and Revenue by Category

--- index.js
+++ index.js
@@ -238,6 +238,47 @@ async function run() {
       const revenue = result.length > 0 ? result[0].totalRevenue : 0;
       res.send({ users, menuItems, orders, revenue });
     });
+    app.get("/order-stats", async (req, res) => {
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $unwind: "$menuItemIds",
+          },
+          {
+            $addFields: {
+              menuItemIds: { $toObjectId: "$menuItemIds" },
+            },
+          },
+          {
+            $lookup: {
+              from: "menu",
+              localField: "menuItemIds",
+              foreignField: "_id",
+              as: "menuItems",
+            },
+          },
+          {
+            $unwind: "$menuItems",
+          },
+          {
+            $group: {
+              _id: "$menuItems.category",
+              quantity: { $sum: 1 },
+              revenue: { $sum: "$menuItems.price" },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              category: "$_id",
+              quantity: "$quantity",
+              revenue: "$revenue",
+            },
+          },
+        ])
+        .toArray();
+      res.send(result);
+    });
   } finally {
   }
 }
